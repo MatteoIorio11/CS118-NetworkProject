bb289d475fbf8b1e49379c2dd8ad32d67ab56840
Matteo <matteo.iorio01@gmail.com>
2022-05-11 21:56:03 +0200

    creation of the server

--- ClientServerUDP/Server.py
+++ ClientServerUDP/Server.py
@@ -1,24 +1,36 @@
 import socket as sk
 import time
+import os
+
+class Server:
+    sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+    @staticmethod
+    def open_connection():
+        server_address = ('localhost', 10000)
+        print('\n\r starting up the server on ip : %s  and port : %s' % server_address)
+        sock.bind(server_address)
+        return sock
+
+    def get_files():
+        print('\n\r Received command : "list files" ')
+        list_directories = os.listdir()
+        directories = ''.join([ str(directory) for directory in list_directories])
+        sock
+
 
 # Creiamo il socket
-sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+    def ciclo(self):
+    while True:
+        print('\n\r waiting to receive message...')
+        data, address = sock.recvfrom(4096)
+        print('received %s bytes from %s' % (len(data), address))
+        print(data.decode('utf8'))
 
-# associamo il socket alla porta
-server_address = ('localhost', 10000)
-print('\n\r starting up on %s port %s' % server_address)
-sock.bind(server_address)
+        if data:
+            data1 = 'Programmazione di Reti'
+            time.sleep(2)
+            sent = sock.sendto(data1.encode(), address)
+            print('sent %s bytes back to %s' % (sent, address))
 
-while True:
-    print('\n\r waiting to receive message...')
-    data, address = sock.recvfrom(4096)
 
-    print('received %s bytes from %s' % (len(data), address))
-    print(data.decode('utf8'))
-
-    if data:
-        data1 = 'Programmazione di Reti'
-        time.sleep(2)
-        sent = sock.sendto(data1.encode(), address)
-        print('sent %s bytes back to %s' % (sent, address))
 
---
+++ ClientServerUDP/main.py
@@ -0,0 +1,11 @@
+
+
+import Server as server
+
+
+def main():
+    socket = server.open_connection()
+
+
+if __name__ == "__main__":
+    main()
