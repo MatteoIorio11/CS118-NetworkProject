c113b1cf80b8dbddc2da2dd10f2193dcda37401b
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-05-14 10:29:04 +0200

    client class

--- ClientServerUDP/Client.py
+++ ClientServerUDP/Client.py
@@ -1,20 +1,28 @@
 import socket as sk
 import time
+import json
 
 class Client:
     def __init__(self):
         self.sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
     
-    def send(self, message, server_address, port):
-        # inviate il messaggio
-        print ('sending "%s"' % message)
-        time.sleep(2) #attende 2 secondi prima di inviare la richiesta
-        sent = sock.sendto(message.encode(), (server_address, port))
+    def set_server_adress(self, server_address, port):
+        self.server_address = server_address
+        self.port = port
         
-        # Ricevete la risposta dal server
+    def get_files_on_server(self):
+        header = {"operation": 1, "status": True}
+        data = json.dumps(header)
+        self.send(data)
+        
+    
+    def send(self, message):
+        print ('sending "%s"' % message)
+        sent = sock.sendto(message.encode(), (self.server_address, self.port))
+        
         print('waiting to receive from')
         data, server = sock.recvfrom(4096)
-        return data.decode('utf8')
+        return data
 
 
 
@@ -24,8 +32,9 @@ message = 'Questo è il corso di ?'
 
 try:
 
+    client.open_connection('localhost', 20000)
     # inviate il messaggio
-    data = client.send(message, 'localhost', 20000)
+    data = client.send(message)
     #print(server)
     time.sleep(2)
     print ('received message "%s"' % data)
--- ClientServerUDP/main.py
+++ ClientServerUDP/main.py
@@ -1,10 +1,9 @@
-
-
-import Server as server
+from Server import Server
 
 
 def main():
-    socket = server.open_connection()
+    server = Server('localhost', 20000)
+    server.open_connection()
 
 
 if __name__ == "__main__":
