3d422450f65b22d11a16c34b424be6d4a228c203
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-05-14 09:56:21 +0200

    creation of Client class

--- ClientServerUDP/Client.py
+++ ClientServerUDP/Client.py
@@ -1,26 +1,34 @@
-
 import socket as sk
 import time
 
-# Create il socket UDP
-sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+class Client:
+    def __init__(self):
+        self.sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+    
+    def send(self, message, server_address, port):
+        # inviate il messaggio
+        print ('sending "%s"' % message)
+        time.sleep(2) #attende 2 secondi prima di inviare la richiesta
+        sent = sock.sendto(message.encode(), (server_address, port))
+        
+        # Ricevete la risposta dal server
+        print('waiting to receive from')
+        data, server = sock.recvfrom(4096)
+        return data.decode('utf8')
 
-server_address = ('localhost', 10000)
+
+
+client = Client()
+
 message = 'Questo è il corso di ?'
 
 try:
 
     # inviate il messaggio
-    print ('sending "%s"' % message)
-    time.sleep(2) #attende 2 secondi prima di inviare la richiesta
-    sent = sock.sendto(message.encode(), server_address)
-
-    # Ricevete la risposta dal server
-    print('waiting to receive from')
-    data, server = sock.recvfrom(4096)
+    data = client.send(message, 'localhost', 20000)
     #print(server)
     time.sleep(2)
-    print ('received message "%s"' % data.decode('utf8'))
+    print ('received message "%s"' % data)
 except Exception as info:
     print(info)
 finally:
