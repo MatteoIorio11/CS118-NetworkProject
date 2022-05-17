0df1e9b2188ba4d6b86189399ddf6a6fa335d177
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-05-17 10:57:00 +0200

    commenting client code

--- ClientServerUDP/Client/Client.py
+++ ClientServerUDP/Client/Client.py
@@ -14,36 +14,38 @@ class Client:
         self.sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
     
     def set_server_adress(self, server_address, port):
+        #storing the IP and Port of the server
         self.server_address = server_address
         self.port = port
         
     def get_files_on_server(self):
-        header = HeaderBuilder.build_header(Operation.GET_FILES.value, True, "", 0, "".encode())
+        header = HeaderBuilder.build_header(Operation.GET_FILES.value, True, "", 0, "".encode())    #create header for getting files
         self.send(header)
         data = self.sock.recv(4096)
         data_json = json.loads(data.decode())
-        if not data_json['status'] :
+        if not data_json['status'] :    #if something whent wrong
             raise Exception(base64.b64decode(data_json['metadata']))
-        files = base64.b64decode(data_json['metadata'])
+        files = base64.b64decode(data_json['metadata'])    #decode files name
         return files.decode()
         
     def get_menu(self):
-        header = HeaderBuilder.build_header(Operation.OPEN_CONNECTION.value, True, "", 0, "".encode())
+        header = HeaderBuilder.build_header(Operation.OPEN_CONNECTION.value, True, "", 0, "".encode())    #create header for getting the menu
         self.send(header)
         data = self.sock.recv(4096)
         data_json = json.loads(data.decode())
-        if not data_json['status'] :
+        if not data_json['status'] :    #if something whent wrong
             raise Exception(base64.b64decode(data_json['metadata']))
-        menu = base64.b64decode(data_json['metadata'])
+        menu = base64.b64decode(data_json['metadata'])    #decode menu
         return menu.decode()
     
     def download_file(self, file_name):
-        header = HeaderBuilder.build_header(Operation.DOWNLOAD.value, True, file_name, 0, "".encode())
+        header = HeaderBuilder.build_header(Operation.DOWNLOAD.value, True, file_name, 0, "".encode())    #create header for downloading a file
         self.send(header)
-        ack = self.sock.recv(4096)
+        ack = self.sock.recv(4096)    #waiting for an ack from the server
         ack_json = json.loads(ack.decode())
         if not ack_json['status'] or ack_json['operation'] != Operation.ACK.value:
             raise Exception(base64.b64decode(ack_json['metadata']).decode())
+        #now the server is ready to send packets and the client to receive them
         tot_packs = float(base64.b64decode(ack_json['metadata']).decode())
         buffer_size = 12_000
         cont_packs = 0
@@ -52,29 +54,30 @@ class Client:
                 data = self.sock.recv(buffer_size)
                 data_json = json.loads(data.decode())
                 cont_packs += 1
-                percent = int(cont_packs*100/tot_packs)
-                print("{:02d}".format(percent), "%", end='\r')       
+                percent = int(cont_packs*100/tot_packs)    #calculating percentage of downloaded packet
+                print("{:02d}".format(percent), "%", end='\r')    #printing percentage    
                 if not data_json['status']:
                     raise Exception(base64.b64decode(data_json['metadata']))
                 if data_json['operation'] == Operation.END_FILE.value:
                     break
                 file = base64.b64decode(data_json['metadata'])
-                f.write(file)
+                f.write(file)    #write the read bytes
     
     def upload(self, file):
-        upload_request = HeaderBuilder.build_header(Operation.UPLOAD.value, True, file, 0, "".encode())
+        upload_request = HeaderBuilder.build_header(Operation.UPLOAD.value, True, file, 0, "".encode())    #creating header for uploading file
         self.send(upload_request)
-        ack = self.sock.recv(4096)
+        ack = self.sock.recv(4096)    #waiting for an ack from the server
         ack_json = json.loads(ack.decode())
         if not ack_json['status'] or ack_json['operation'] != Operation.ACK.value:
             raise Exception(base64.b64decode(ack_json['metadata']))
+        #now the client is ready to send packets and the server to receive them
         buffer_size = 8192
         cont_packs = 0
         if file in os.listdir(os.getcwd()):
             tot_packs = os.path.getsize(os.path.join(os.getcwd(), file))/buffer_size
             print('\n\r Sending the file %s to the destination', str(file))
             with open(os.path.join(os.getcwd(), file), 'rb') as handle:
-                byte = handle.read(buffer_size)   # Read a buffer size
+                byte = handle.read(buffer_size)   #Read buffer_size bytes from the file
                 cont_packs += 1
                 while byte:
                     header = HeaderBuilder.build_header(Operation.UPLOAD.value, True, file, buffer_size, byte)  # Send the read bytes to the Client
@@ -83,9 +86,9 @@ class Client:
                     percent = int(cont_packs*100/tot_packs)
                     print("{:03d}".format(percent), "%", end='\r')
                     time.sleep(0.001)
-                    byte = handle.read(buffer_size)   # Read a buffer size
+                    byte = handle.read(buffer_size)   #Read buffer_size bytes from the file
             header = HeaderBuilder.build_header( Operation.END_FILE.value, True,
-                                                 "", 0, "".encode())  # Send the bytes read to the Client
+                                                 "", 0, "".encode())  #Telling to the server that the file is complete
             self.send(header)
         else:
             print("The input file does not exit!")
