7ec07e088afb1fbaf1dfee7c80a25d0f6e19406f
Matteo <matteo.iorio01@gmail.com>
2022-05-19 22:26:38 +0200

    add comments

--- ClientServerUDP/FileNameFactory.py
+++ ClientServerUDP/FileNameFactory.py
@@ -1,15 +1,22 @@
 import os
+
+
+# This class is used in order to get the file's name in the Server's directory.
+# If a client send a file (ex.txt) and this file is already in the Server's directory,
+# the File will be named ex(1).txt
 class FileNameFactory:
     @staticmethod
     def get_file_name(file, file_name, path):
         fk = file_name
         cont = 1
         for file in os.listdir(path):
-            file = file.split('.')[0]
+            file = file.split('.')[0] # A.txt -> A
             if file.__contains__('('):
+                # Get the left part of the file's name
                 file = file.split('(')[0] + '.' + file_name.split('.')[1]
                 if file == file_name:
-                    cont = cont +1
+                    # Is the same file, update the cont
+                    cont = cont + 1
                 elif file_name.__contains__('('):
                     f_tmp = file_name.split('(')[0] + '.' + file_name.split('.')[1]
                     if f_tmp == file:
@@ -18,4 +25,4 @@ class FileNameFactory:
                 cont = cont + 1
         if cont > 1:
             file_name = fk.split('.')[0] + '(' + str(cont) + ').' + fk.split('.')[1]
-        return file_name;
+        return file_name
